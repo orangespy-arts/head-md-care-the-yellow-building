24252360973a04f01c34859c797569c8f4e21061
head-imac <zhanlan0w0@gmail.com>
2026-05-22 14:21:09 +0200

    add dance animation

---
+++ unity/GreyBoxing/Assets/Scripts/complain.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class complain : MonoBehaviour, IPointerClickHandler
+{
+    // create a public list of strings called complaints
+    public string[] complaints;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        
+    }
+
+}
